973dd31c974e9f4ac6263c67e228bfd86f7e429c
msousa06 <msousa06@users.noreply.github.com>
2017-11-21 21:43:42 +0000

    Nivel 3 concluido

--- lab05/Models/Carro.cs
+++ lab05/Models/Carro.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +10,11 @@ namespace lab05.Models
     public class Carro
     {
         public int CarroId { get; set; }
-        public String Modelo { get; set; }
-        public int NumeroDePassageiros { get; set; }
-        public int NumeroDePortas { get; set; }
-        public int EmissoesCO2 { get; set; }
-        public String TipoCaixa { get; set; }
+        [Required] [StringLength(20)] public String Modelo { get; set; }
+        [DisplayName("Número de passageiros")][Range (2,9)]public int NumeroDePassageiros { get; set; }
+        [DisplayName("Número de portas")] [Range(2,5)] public int NumeroDePortas { get; set; }
+        [DisplayName("Emissões de CO2")] public int EmissoesCO2 { get; set; }
+        [DisplayName("Tipo de Caixa")] public String TipoCaixa { get; set; }
         public int MarcaId { get; set; }
 
         public virtual Marca Marca { get; set; }
--- lab05/Models/Marca.cs
+++ lab05/Models/Marca.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,7 @@ namespace lab05.Models
     public class Marca
     {
         public int MarcaId { get; set; }
-        public String Designacao { get; set; }
+        [DisplayName("Designação")] [Required] [StringLength(15)]public String Designacao { get; set; }
 
         public virtual ICollection<Carro> Carros { get; set; } 
     }
